eab380e21de0bf823e0bf7548e04e2f8a3f7f59a
Greg Orvik <gvorvik@gmail.com>
2018-04-26 08:50:52 -0500

    serve the public folder at local host 5000

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
+app.use(bodyParser.urlencoded({extended:true})); 
+app.use(express.static('server/public'));
+
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
