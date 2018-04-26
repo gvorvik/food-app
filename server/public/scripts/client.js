8be977d73fcd6d723402b456732c563efa3e8c3c
Greg Orvik <gvorvik@gmail.com>
2018-04-26 08:48:44 -0500

    source in angularjs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,8 @@
 console.log('Client.js is loaded');
 
+var app = angular.module('FoodApp', []);
 
+app.controller('FoodAppController', function() {
+    var self = this;
+    self.message = "Hello World";
+});
