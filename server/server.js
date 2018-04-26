98d952d8d68ce4f468290717356321a059df83c3
Greg Orvik <gvorvik@gmail.com>
2018-04-26 09:01:17 -0500

    receive get request at /foods

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static('server/public'));
 
+app.get('/foods', (req, res) => {
+    res.send('Get request Got');
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
